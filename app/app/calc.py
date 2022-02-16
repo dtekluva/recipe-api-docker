374c60290d80a1e2d3c8c3360b2b5d4c896bf626
dtekluva <kboysreel@gmail.com>
2022-02-16 07:46:36 +0100

    FLAKE8 CLSERTIFIED

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,9 +5,10 @@ REQUIRES DOCSTRING
 
 
 def add(x, y):
-    """ADD 2 NUMBER TOGETHER"""
+    """ADD 2 NUMBER TOGETHER."""
     return x + y
 
+
 def subtract(x, y):
-    """Subtract 2 NUMBER  TOGETHER"""
+    """Subtract 2 NUMBER  TOGETHER."""
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -7,15 +7,14 @@ from django.test import TestCase
 
 from app.calc import add, subtract
 
+
 class CalcTests(TestCase):
-
-
     """DJANGO TESTS."""
-    
+
     def test_add_numbers(self):
-        """Tests that two numbers are added together"""
-        self.assertEqual(add(3,8),11)
+        """Tests that two numbers are added together."""
+        self.assertEqual(add(3, 8), 11)
 
     def test_subtract_numbers(self):
-        """Test that values are subtracted and returned"""
-        self.assertEqual(subtract(5,11),-6) 
+        """Test that values are subtracted and returned."""
+        self.assertEqual(subtract(5, 11), -6)
